e17e3dad59eb96c07f4b60706c5bd17e21f4b7fa
tunay <jtunay@gmail.com>
2020-10-08 17:49:59 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/example/demoProjectOne/DemoProjectOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoProjectOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoProjectOne/DemoProjectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoProjectOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
